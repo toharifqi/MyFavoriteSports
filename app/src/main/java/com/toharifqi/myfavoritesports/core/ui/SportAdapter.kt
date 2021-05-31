979842141503c4b5b86f837a03dce5f571930cae
Rifqi Naufal Tohari <44081111+toharifqi@users.noreply.github.com>
2021-05-31 19:27:36 +0700

    Delete app/src/main/java/com/toharifqi/myfavoritesports/core/ui directory

--- app/src/main/java/com/toharifqi/myfavoritesports/core/ui/SportAdapter.kt
+++
@@ -1,8 +0,0 @@
-package com.toharifqi.myfavoritesports.core.ui
-
-import androidx.recyclerview.widget.RecyclerView
-
-
-class SportAdapter : RecyclerView.Adapter<SportAdapter.ListViewHolder>(){
-
-}
